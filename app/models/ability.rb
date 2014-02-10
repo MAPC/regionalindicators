316f6f40494752ae7dbd421f684a2739af3ebe30
Matt Cloyd <cloydster@gmail.com>
2014-02-10 12:35:11 -0500

    Make failed login redirect to login page. Previously it was redirecting to root, making multiple attempts harder.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,10 +2,9 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-
     can :read, :all
+    can :access, :rails_admin
     if user && user.admin?
-      can :access, :rails_admin
       can :dashboard
       can :manage, :all
     end
