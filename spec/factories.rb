aedd57c2f46f8aec2f94adf9ca4caa66498b2ce1
Matt Cloyd <cloydster@gmail.com>
2014-02-18 14:41:20 -0500

    Finish up leftover & pending tests

--- spec/factories.rb
+++ spec/factories.rb
@@ -14,5 +14,18 @@ FactoryGirl.define do
     sort_order 1
   end
 
+  factory :user do
+    password  "lolZ1293!?"
+    password_confirmation "lolZ1293!?"
+    email     "user@mapc.org"
+    admin     false
+  end
+
+  factory :indicator do
+    title  "An Indicator to Remember"
+    number "20"
+    units  "percentage points"
+  end
+
 end
 
