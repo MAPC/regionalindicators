e284ffba0e5ffceb11f531484094acd2006de687
Matt Cloyd <cloydster@gmail.com>
2014-03-26 19:49:12 -0400

    Change port and group

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,7 +1,7 @@
-server '10.10.10.89', :app, :web, :db, primary: true
+Couserver '10.10.10.89', :app, :web, :db, primary: true
 
 set :user,      'deployer'
-set :group,     'admin'
+set :group,     'staff'
 
 set :branch,    'origin/master'
 set :deploy_to, "/home/#{user}/apps/#{stage}.#{application}"
