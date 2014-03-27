1e3b58eef47a35f03834ff76f4b2f432efe6d258
Matt Cloyd <cloydster@gmail.com>
2014-03-27 14:45:58 -0400

    Rename socket

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -7,7 +7,7 @@ worker_processes 4
 
 # listen on both a Unix domain socket and a TCP port,
 # we use a shorter backlog for quicker failover when busy
-listen '/tmp/regionalindicators.socket', backlog: 64
+listen '/tmp/staging.regionalindicators.socket', backlog: 64
 listen 8010
 
 # Preload our app for more specified
