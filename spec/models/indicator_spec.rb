73a4e0d91597f8450e3087f723b414d835d4fffd
Matt Cloyd <cloydster@gmail.com>
2014-04-17 21:47:15 -0400

    Added a lot of helper methods to check for trends in value, rank and whether or not indicator is passing

--- spec/models/indicator_spec.rb
+++ spec/models/indicator_spec.rb
@@ -38,6 +38,23 @@ describe Indicator do
   it { should respond_to :value }
   it { should respond_to :rank }
 
+  it { should respond_to :threshhold }
+  it { should respond_to :higher_value_is_better }
+  it { should respond_to :lower_rank_is_better  }
+
+  it { should respond_to :passing? }
+  it { should respond_to :failing? }
+
+  it { should respond_to :improving? }
+  it { should respond_to :rank_improving? }
+
+  it { should respond_to :stagnant? }
+  it { should respond_to :rank_stagnant? }
+  it { should respond_to :stagnant_rank? }
+
+  it { should respond_to :trend }
+  it { should respond_to :rank_trend }
+
   describe "when title is not present" do
     before { @indicator.title = " " }
     it { should_not be_valid }
@@ -121,10 +138,179 @@ describe Indicator do
 
     describe "#rank_delta" do
       it "gives the difference in year ranks" do
-        @indicator.rank_delta(2000).should == 13
+        @indicator.rank_delta(2000).should == -13
       end
     end
   end
+
+  describe "passing and failing values" do
+    describe "with a threshhold" do
+
+      before do
+        @indicator.threshhold = 100
+        @indicator.save
+      end
+
+      describe "with a normal orientation" do
+        before { @indicator.snapshots.create(date: Time.now, rank: 1, value: 101) }
+        
+        it "passes and does not fail" do
+          @indicator.passing?.should == true
+          @indicator.failing?.should == false
+        end
+      end
+
+      describe "with a reverse orientation" do
+        before do
+          @indicator.higher_value_is_better = false
+          @indicator.snapshots.create(date: Time.now, rank: 1, value: 101)
+        end
+        
+        it "fails and does not pass" do
+          @indicator.passing?.should == false
+          @indicator.failing?.should == true
+        end
+      end
+    end
+
+    describe "without a threshhold" do
+      before do
+        @indicator.threshhold = nil
+        @indicator.save
+      end
+    end
+  end
+
+
+  describe "value and rank delta judgment" do
+
+    before { @indicator.save }
+
+    describe "with no snapshots" do
+
+      describe "#passing" do
+        it "returns nil" do
+          @indicator.passing?.should == nil
+        end
+      end
+
+      describe "#failing" do
+        it "returns nil" do
+          @indicator.failing?.should == nil
+        end
+      end
+
+      describe "#improving?" do
+        its(:improving?) { should == nil }
+      end
+
+      describe "#stagnant?" do
+        its(:stagnant?) { should == nil }
+      end
+
+      describe "#rank_improving?" do
+        its(:rank_improving?) { should == nil }
+      end
+
+      describe "#rank_stagnant?" do
+        its(:rank_stagnant?) { should == nil }
+      end
+
+      describe "#trend" do
+        its(:trend) { should == nil }
+      end
+
+      describe "#rank_trend" do
+        its(:rank_trend) { should == nil }
+      end
+
+    end
+
+    describe "with improving values" do
+
+      let!(:snap) { @indicator.snapshots.create( date: Date.new(2000), rank: 2, value: 1 ) }
+      let!(:pans) { @indicator.snapshots.create( date: Time.now,       rank: 1, value: 2 ) }
+
+      describe "with a normal orientation" do
+
+        describe "value delta" do
+          it "is improving" do
+            @indicator.improving?.should == true
+          end
+
+          it "has an improving trend" do
+            @indicator.trend.should == :improving
+          end
+        end
+
+        describe "rank delta" do
+          it "is improving" do
+            @indicator.rank_improving?.should == true
+          end
+
+          it "has an improving rank trend" do
+            @indicator.rank_trend.should == :improving
+          end
+        end
+      end
+
+      describe "with a reverse orientation" do
+
+        before do
+          @indicator.update_attribute(:higher_value_is_better, false)
+          @indicator.update_attribute(:lower_rank_is_better,   false)
+        end
+
+        describe "value delta" do
+          it "is not improving" do
+            @indicator.improving?.should == false
+          end
+          it "has a declining trend" do
+            @indicator.trend.should == :declining
+          end
+        end
+
+        describe "rank delta" do
+          it "is not improving" do
+            @indicator.rank_improving?.should == false
+          end
+          it "has a declining rank trend" do
+            @indicator.rank_trend.should == :declining
+          end
+        end
+      end
+    end
+
+    describe "with stagnant values" do
+
+      let!(:snap) { @indicator.snapshots.create( date: Date.new(2000), rank: 1, value: 2 ) }
+      let!(:pans) { @indicator.snapshots.create( date: Time.now,       rank: 1, value: 2 ) }
+
+      describe "has stagnant" do
+
+        its "value" do
+          @indicator.stagnant?.should == true
+        end
+
+        its "rank" do
+          @indicator.rank_stagnant?.should == true
+        end
+      end
+
+      it "has a stagnant trend" do
+        @indicator.trend.should == :stagnant
+      end
+
+      it "has a stagnant rank trend" do
+        @indicator.rank_trend.should == :stagnant
+      end
+
+    end
+
+  end
+
+
+
 
 
 
