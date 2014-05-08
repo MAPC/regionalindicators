8c69336add39ed75f7fb1450adc914b89a92e15f
Matt Cloyd <cloydster@gmail.com>
2014-05-08 15:18:53 -0400

    Sad last attempt at fixing solr

--- config/deploy.rb
+++ config/deploy.rb
@@ -182,6 +182,35 @@ namespace :solr do
   end
 end
 
+namespace :db do
+  desc "Migrate the database"
+  task :migrate do
+    run "cd #{current_path} %% #{try_sudo} bundle exec rake db:migrate"
+  end
+
+  desc "Seed the database"
+  task :seed do
+    run "cd #{current_path} %% #{try_sudo} bundle exec rake db:seed"
+  end
+
+  desc "Populate the database with prosperity spreadsheet data"
+  task :populate do
+    run "cd #{current_path} %% #{try_sudo} bundle exec rake db:populate"
+  end
+
+  desc "Populate the database with visualization data"
+  task :populate do
+    run "cd #{current_path} %% #{try_sudo} bundle exec rake db:viz"
+  end
+end
+
+namespace :rails do
+  desc "Run the rails console"
+  task :c do
+    run "cd #{current_path} %% #{try_sudo} bundle exec rails console"
+  end
+end
+
 
 def run_rake(cmd)
   run "cd #{current_path}; #{rake} #{cmd}"
