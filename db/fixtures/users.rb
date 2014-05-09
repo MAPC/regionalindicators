a39c5d44abdcd7305ccfa9e42ad32ba3e8888b15
Matt Cloyd <cloydster@gmail.com>
2014-05-08 21:46:09 -0400

    No users in seeds.

--- db/fixtures/users.rb
+++ db/fixtures/users.rb
@@ -1,9 +1,6 @@
 module Fixtures
   def users
-    [{ id: 1, email: 'mcloyd@mapc.org',            password: 'password' },
-     { id: 2, email: 'grimley@overcommaunder.com', password: 'password' },
-     { id: 3, email: 'hstclair@mapc.org',          password: 'password' },
-     { id: 4, email: 'jpartridge@mapc.org',        password: 'password' }]
+    {}
   end
 
   module_function :users
