e4b2ab33f8d0498cc076ec5274d7e2e41704b512
allthesignals <wilburnforce@gmail.com>
2015-03-30 15:21:53 -0400

    Adding fix as a required lib.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,21 +10,6 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-if(navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
-
-    document._oldGetElementById = document.getElementById;
-    document.getElementById = function(id) {
-
-        if(id === undefined || id === null || id === '') {
-
-            return undefined;
-
-        }
-
-        return document._oldGetElementById(id);
-    };
-
-}
 //= require lib/bootstrap-fix.js
 //= require jquery
 //= require jquery_ujs
