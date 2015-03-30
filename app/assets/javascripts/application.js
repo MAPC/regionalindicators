2e1996de5a0a383ba52f6d9b8b57802123229e0d
allthesignals <wilburnforce@gmail.com>
2015-03-30 16:29:50 -0400

    Pulling a different version of bootstrap from CDN because the gem-provided bootstrap conflicts arbitrarily with other certain dependency requirements. Avoid Bootstrap in the future.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,8 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require lib/bootstrap-fix.js
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap.min
 //= require twitter/typeahead.min
 //= require d3.v3
 //= require miso.ds.deps.ie.0.4.1
